b41cb2532eeecf51af72de8b24a24eb8bf7f436b
Joan' Araujo <wisdombthegame@gmail.com>
2020-06-25 18:57:59 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,6 @@
-def position_taken?
+def position_taken?(board)
+  
 
 end
 
+#This method will be responsible for evaluating the user's input against the Tic Tac Toe board and checking to see whether or not that position is occupied.
