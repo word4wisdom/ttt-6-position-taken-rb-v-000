d60beba209b6f5d12ca2cfdd8ecd437c83d64661
Joan' Araujo <wisdombthegame@gmail.com>
2020-06-25 19:13:05 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,10 +4,10 @@ def position_taken?(board, index)
 end
 
 
-board[index] == " " || ""  ? :
+board[index] == " " || ""  ? false : true
 
 
-
+apple_stock > 1 ? :eat_apple : :buy_appl
 
 #This method will be responsible for evaluating the user's input against the Tic Tac Toe board and checking to see whether or not that position is occupied.
 #position_taken?` and check to see if that position is vacant or if it contains an "X" or an "O". If the position is free, the method should return `false` (i.e. "not taken"); otherwise it will return `true`.+
